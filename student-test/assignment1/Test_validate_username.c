6d461715d3eb93a3905e99845e7ba8e9e52848a8
isch4196 <isch4196@colorado.edu>
2023-01-25 12:26:22 -0800

    Finished unity username test part

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,13 @@
 */
 void test_validate_my_username()
 {
+  const char* username1 = my_username();
+  char* username2 = malloc_username_from_conf_file();
+
+  TEST_ASSERT_EQUAL_STRING_MESSAGE(username1, username2, "Test failed!");
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
