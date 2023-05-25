6f421eb2fa5c133efb6ae7e467dcb3664ec2c889
isch4196 <isch4196@colorado.edu>
2023-05-25 14:54:05 -0700

    Passed full-test, but corner cases may still exist

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -11,6 +11,7 @@
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
+#include <stdio.h>
 #include <string.h>
 #endif
 
@@ -29,10 +30,18 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    size_t tot_size = 0;
+    uint8_t i = buffer->out_offs;
+    do {
+	tot_size += buffer->entry[i].size;
+	if(char_offset < tot_size) {
+	    *entry_offset_byte_rtn = char_offset - (tot_size - buffer->entry[i].size);
+	    return &buffer->entry[i];
+	}
+	i = (i+1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    } while(i != buffer->in_offs);
     return NULL;
+
 }
 
 /**
@@ -44,9 +53,20 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    if(true == buffer->full) {
+	buffer->entry[buffer->in_offs] = *add_entry;
+	buffer->in_offs += 1;
+	buffer->out_offs += 1;
+    } else {
+	buffer->entry[buffer->in_offs] = *add_entry;
+	buffer->in_offs += 1;
+	if(buffer->in_offs >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+	    buffer->in_offs = 0;
+	}
+	if(buffer->in_offs == buffer->out_offs) {
+	    buffer->full = true;
+	}
+    }
 }
 
 /**
@@ -56,3 +76,4 @@ void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
 }
+
